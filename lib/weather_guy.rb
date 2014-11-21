859d123b41a2b76b18b5da073f72b235ee7f0664
Jay Wengrow <jaywngrw@gmail.com>
2014-11-20 18:47:30 -0600

    abstracted logic into separate class

--- lib/weather_guy.rb
+++ lib/weather_guy.rb
@@ -1,5 +1,13 @@
 require "weather_guy/version"
 
 module WeatherGuy
-  # Your code goes here...
+  class Converter
+    def self.convert_from_f_to_c(f_temp)
+      (f_temp.to_i - 32) * 5 / 9
+    end
+
+    def self.convert_from_f_to_k(f_temp)
+      (f_temp.to_i + 459.67) * 5 / 9
+    end
+  end
 end
